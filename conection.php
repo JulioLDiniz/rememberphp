99f1b55e525e52946df6fc0187bcf2228581ed28
Júlio Ítalo Liarte Diniz <academicojulio@gmail.com>
2020-02-04 22:22:25 -0300

    Add files via upload

--- conection.php
+++ conection.php
@@ -23,29 +23,53 @@
 
 	class Model
 	{
-
-			 
-
-		
-
 		public function listaTudo()
 		{
-			$conectar = new Conexao();
-			$con = $conectar->conectar();
+			$conexao = new Conexao();
+			$con = $conexao->conectar();
 			try {
-				$geral = $con->query("select * from maquinas");
-				return $geral->fetchAll(PDO::FETCH_ASSOC);
+				$list = $con->query("select * from maquinas");
+				return $list->fetchAll(PDO::FETCH_ASSOC);
 			} catch (Exception $e) {
 				echo "algo deu errado ".$e;
 			}
 		}
 
+		public function listaUm($id)
+		{
+			$conexao = new Conexao();
+			$con = $conexao->conectar();
+			try {
+				$line = $con->prepare("select * from maquinas where id = :id");
+				$line->bindParam(":id", $id);
+				$line->execute();
+				return $line->fetchAll();
+			} catch (Exception $e) {
+				echo "houve um erro".$e;
+			}
+		}
+
+		public function update($id, array $dados)
+		{
+			$conexao = new Conexao();
+			$con = $conexao->conectar();
+			try {
+				$update = $con->prepare("update maquinas set ip = :ip, nome = :nome where id = :id");
+				$update->bindParam(":ip", $dados["ip"]);
+				$update->bindParam(":nome", $dados["nome"]);
+				$update->bindParam(":id", $id);
+				$update->execute();
+				return "Suuuucesso!";
+			} catch (Exception $e) {
+				echo "houve um erro".$e;
+			}
+		}
 
 	}
 
-	// $model = new Model();
+	$model = new Model();
 
-	// var_dump($model->listaTudo());
+	var_dump($model->update(2, ["ip"=>"192", "nome"=>"uma nova maquina"]));
 
 	
 
